5ea0af997a6d3a789f51dc7384873ae6bfb67806
whiskeeyyy <p3ddii@hotmail.com>
2024-09-24 15:01:39 +0200

    added random data generation to testdata, worked on the first and second test case, merged helpers back to one file, added CRUD to orders. Sebastian was live coding with me for both commits, we will likely go back to working locally on separate machines now.

--- tests/testsuite1.spec.ts
+++ tests/testsuite1.spec.ts
@@ -1,18 +1,64 @@
 import { test, expect } from '@playwright/test';
+import { APIHelper } from './apiHelpers';
+import { generateRandomCustomerPayload } from './testData';
+import { generateRandomCarPayload } from './testData';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Test suite 1 backend', () => {
+  let apiHelper: APIHelper;
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test.beforeAll(() => {
+    apiHelper = new APIHelper(`${process.env.BASE_URL}`); //if this doenst work replace with only BASE_URL
+  })
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test('Test case 01 - Get all customers', async ({ request }) => {
+    const getCustomers = await apiHelper.getAllCustomers(request);
+    expect(getCustomers.ok()).toBeTruthy();
+    expect(getCustomers.status()).toBe(200);
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test('Test case 02 - create random customer', async ({ request }) => {
+    const payload = generateRandomCustomerPayload();
+    const createCustomer = await apiHelper.createCustomer(request, payload);
+    //expect(createCustomer.ok()).toBeTruthy();
+    //expect(createCustomer.status()).toBe(200);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+    // verifying from the POST requestr
+    expect(await createCustomer.json()).toMatchObject({
+      username: payload.username,
+      name: payload.name,
+      adress:payload.address,
+      email:payload.email,
+      phoneNumber:payload.phoneNumber,
+    })
+
+    // verifying from GET request 
+    const getCustomers = await apiHelper.getAllCustomers(request);
+    expect(getCustomers.ok()).toBeTruthy();
+    expect(await getCustomers.json()).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          username: payload.username,
+          name: payload.name,
+          adress:payload.address,
+          email:payload.email,
+          phoneNumber:payload.phoneNumber,
+        })
+      ])
+    )
+  });
+
+  /*test('Test case 03 - Delete Post - v2', async ({ request }) => {
+    const getPosts = await apiHelper.getAllPosts(request);
+    expect(getPosts.ok()).toBeTruthy();
+    const allPosts = await getPosts.json();
+    const lastButOneID = allPosts[allPosts.length - 2].id;
+
+    //Delete request
+    const deleteRequest = await apiHelper.deletePost(request, lastButOneID);
+    expect(deleteRequest.ok()).toBeTruthy();
+
+    // GET by ID and verify status as 404
+    const getPostById = await apiHelper.getByID(request, lastButOneID);
+    expect(getPostById.status()).toBe(404);
+  });  */
+})
